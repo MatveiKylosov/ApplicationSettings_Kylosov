27a4892f2780ced9df75c51f2442870ea3c363c2
MatveiKylosov <matveikyl@mail.ru>
2023-12-01 22:12:32 +0500

    Разработанная страница настроек. Кылосов

--- Pages/Settings.xaml.cs
+++ Pages/Settings.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,6 +8,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -20,9 +22,34 @@ namespace ApplicationSettings_Kylosov.Pages
     /// </summary>
     public partial class Settings : Page
     {
-        public Settings()
+        System.Windows.Forms.OpenFileDialog openFileDialog = new System.Windows.Forms.OpenFileDialog();
+        ColorDialog colorDialog = new ColorDialog();
+        public Settings(MainWindow _mainWindow)
         {
             InitializeComponent();
+
+            openFileDialog.InitialDirectory = "c:\\";
+            openFileDialog.Filter = "Access files (*.accdb)|*.accdb|All files (*.*)|*.*";
+            openFileDialog.FilterIndex = 2;
+            openFileDialog.RestoreDirectory = true;
+
+            colorDialog.AllowFullOpen = true;
+            colorDialog.ShowHelp = false;
+        }
+        private void OpenDataBase(object sender, RoutedEventArgs e)
+        {
+            if(openFileDialog.ShowDialog() = DialogResult.OK)
+                tb_database.Text = openFileDialog.FileName;
+        }
+        
+        private void SelectColorApplication(object sender, RoutedEventArgs e)
+        {
+            if(colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                System.Drawing.Color color = colorDialog.Color;
+
+                gr_header.Background = new SolidColorBrush();
+            }
         }
     }
 }
